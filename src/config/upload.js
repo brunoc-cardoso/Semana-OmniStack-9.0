9329cdb389fd478d637b22f03ae318bc9ab0a3e3
bruno <brunocpessoal@gmail.com>
2019-10-04 18:55:56 +0000

    added route for spot creation and image upload

---
+++ src/config/upload.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const multer = require('multer');
+const path = require('path');
+
+module.exports = {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'uploads'), 
+    filename: (req, file, callbak) => {
+      const ext = path.extname(file.originalname);
+      const name = path.basename(file.originalname, ext);
+
+      callbak(null, `${name} - ${Date.now()}${ext}`)
+    }
+  })
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const express = require('express');
+const multer = require('multer');
+
 const routes = express.Router();
+const uploadConfig = require('./config/upload');
+const upload = multer(uploadConfig);
 
+// Controllers
 const sessionController = require('../src/controllers/SessionController');
+const spotController = require('../src/controllers/SpotController');
 
+// Routes
 routes.post('/sessions', sessionController.store);
+routes.post('/spots', upload.single('thumbnail'), spotController.store);
 
 module.exports = routes;
