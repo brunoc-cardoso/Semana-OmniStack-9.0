f2989bd23da0c2f6f86c2a6612e903fb1617c734
bruno <brunocpessoal@gmail.com>
2019-10-05 14:01:30 +0000

    added dashboardController

--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,12 @@ const upload = multer(uploadConfig);
 // Controllers
 const sessionController = require('../src/controllers/SessionController');
 const spotController = require('../src/controllers/SpotController');
+const dashboardController = require('../src/controllers/DashboardController');
 
 // Routes
 routes.post('/sessions', sessionController.store);
 routes.post('/spots', upload.single('thumbnail'), spotController.store);
+routes.get('/spots', spotController.index);
+routes.get('/dashboard', dashboardController.show);
 
 module.exports = routes;
