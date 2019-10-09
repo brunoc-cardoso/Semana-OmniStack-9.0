065e287a5a00c9f94659ffa2fe74784bd41b20a9
bruno <brunocpessoal@gmail.com>
2019-10-09 16:43:16 +0000

    added a thumbnails delivery route

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-le6dz.mongodb.net
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(port);
